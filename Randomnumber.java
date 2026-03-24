eb2b544b0060845548794112234f94548ff4a385
Tai sufyaan <sufiyantai05@gmail.com>
2026-03-24 16:45:00 +0530

    Add files via upload

---
+++ Randomnumber.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+public class Randomnumber {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random rm = new Random();
+		int no = rm.nextInt(100)+1;
+		
+		System.out.print("\n No ="+no);
+		
+		
+ 
+		
+
+	}
+
+	
+
+}
